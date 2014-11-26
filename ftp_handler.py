e87c55ad63ffe404ccd604b2dbbe5319f64f3c9e
Podshot <ben.gothard3@gmail.com>
2014-11-26 13:10:56 -0500

    ftp_handler now downloads a entire world folder specified by the server.properties. The world even has valid file data too!

--- ftp_handler.py
+++ ftp_handler.py
@@ -1,24 +1,70 @@
-from ftplib import FTP
+import ftplib
+import os
+import directories
+import shutil
+import time
 
-def read_properties():
-    levelname = "world"
-    with open('server.properties') as props_f:
-        content = []
-        old_content = props_f.readlines()
-        for line in old_content:
-            content.append(line.strip('\n'))
-        for prop in content:
-            if prop.startswith("level-name"):
-                levelname = prop.split("=")[1:][0]
-    print levelname
-    return levelname
+class MCEdit_FTP_Client:
 
-read_properties()
+    def read_properties(self):
+        levelname = "world"
+        with open(directories.getDataDir()+os.path.sep+'ftp-data'+os.path.sep+'server.properties', 'r') as props_f:
+            old_content = props_f.readlines()
+            for prop in old_content[0].split('\r'):
+                if prop.startswith("level-name"):
+                    levelname = prop.split("=")[1:][0]
+        print levelname
+        return levelname
+    
+    def download_folder(self, folder, level):
+        try:
+            os.mkdir(directories.getDataDir()+os.path.sep+'ftp-data'+os.path.sep+level+os.path.sep+folder)
+        except OSError:
+            pass
+        self._ftp.cwd(folder)
+        if self._ftp.nlst()[2:] != []:
+            for item in self._ftp.nlst()[2:]:
+                self._ftp.retrbinary("RETR "+item, open(directories.getDataDir()+os.path.sep+'ftp-data'+os.path.sep+level+os.path.sep+folder+os.path.sep+item, 'wb').write)
+        self._ftp.cwd("..")
+        #time.sleep(10)
+        pass
 
-# Removed FTP class init due to containing my password, it will be re-added once everything is implemented
 
-for f in ftp.nlst():
-    if f == "server.properties":
-        print "Founds Server Properties"
+    def getWorld(self, level):
+        try:
+            os.mkdir(directories.getDataDir()+os.path.sep+'ftp-data'+os.path.sep+level)
+        except OSError:
+            pass
+        self._ftp.cwd(level)
+        found = self._ftp.nlst()
+        for obj in found[2:]:
+            if "." in obj:
+                print obj
+                self._ftp.retrbinary("RETR "+obj, open(directories.getDataDir()+os.path.sep+'ftp-data'+os.path.sep+level+os.path.sep+obj, 'wb').write)
+            else:
+                print obj+" is a folder"
+                self.download_folder(obj, level)
+    
+    
+    def __init__(self, address, username, password):
+        try:
+            os.mkdir(directories.getDataDir()+os.path.sep+'ftp-data')
+        except OSError:
+            pass
+
+        # Removed FTP class init due to containing my password, it will be re-added once everything is implemented
+        self._ftp = ftplib.FTP(address, username, password)
+        for f in self._ftp.nlst():
+            if f == "server.properties":
+                self._ftp.retrlines("RETR server.properties", open(directories.getDataDir()+os.path.sep+'ftp-data'+os.path.sep+'server.properties', 'wb').write)
+                level = self.read_properties()
+                self.getWorld(level)
         
-ftp.quit()
+    def stop(self):
+        shutil.rmtree(directories.getDataDir()+os.path.sep+'ftp-data')
+        self._ftp.quit()
+        
+
+yn = raw_input("CMD: ")
+if yn == "stop":
+    client.stop()  # @UndefinedVariable
